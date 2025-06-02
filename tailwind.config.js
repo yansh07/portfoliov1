65837f1ff440b4a1f607710a636bd10bc8957bd2
pksingh <pksingh69313@gmail.com>
2025-06-02 22:18:32 +0530

    Add a sexy skill strip - you'll love it

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
       fontFamily: {
         heading: ["Montserrat", "sans-serif"],
         body: ["Inter", "sans-serif"],
