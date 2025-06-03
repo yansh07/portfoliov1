f2df6f7e2e5bf9a7fba42d58f22d73008fbca61f
pksingh <pksingh69313@gmail.com>
2025-06-03 21:44:48 +0530

    Added animations, did some responsiveness stuffs, and also added an avatar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,17 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        accent: "teal",
+      },
       keyframes: {
         marquee: {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-50%)" },
+          "0%": { transform: "translateX(100)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
       animation: {
-        marquee: "marquee 20s linear infinite",
+        marquee: "marquee 10s linear infinite",
       },
       fontFamily: {
         heading: ["Montserrat", "sans-serif"],
