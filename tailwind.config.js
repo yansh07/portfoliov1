a751ec6605b4052229ef65e303a5a7df885e7fe3
pksingh <pksingh69313@gmail.com>
2025-07-13 23:57:57 +0530

    Fixed columns on small screens, and background

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,7 +106,7 @@ module.exports = {
           "100%": { transform: "translate(0px, 0px) scale(1)" },
         },
         marquee: {
-          "0%": { transform: "translateX(50%)" },
+          "0%": { transform: "translateX(40%)" },
           "100%": { transform: "translateX(-80%)" },
         },
         blur: {
